cb4f53502325cd93721855aadcc4d162445efd7b
Aoi <ht.so.ik0102@gmail.com>
2021-10-08 20:30:04 +0700

    create some test cases

--- test/test.js
+++ test/test.js
@@ -2,10 +2,10 @@ let Auction = artifacts.require("./Auction.sol");
 
 let auctionInstance;
 
-contract('AuctionContract', function (accounts) {
+contract('AuctionContract', (accounts) => {
   //accounts[0] is the default account
   //Test case 1
-  it("Contract deployment", function() {
+  it("Contract deployment", () => {
     //Fetching the contract instance of our smart contract
     return Auction.deployed().then(function (instance) {
       //We save the instance in a gDlobal variable and all smart contract functions are called using this
@@ -15,118 +15,126 @@ contract('AuctionContract', function (accounts) {
   });
 
   //Sample Test Case
-  it("Should set bidders", function() {
-    return auctionInstance.register({from:accounts[1]}).then(function(result) {
-        return auctionInstance.getPersonDetails(0);
-    }).then(function(result) {
+  it("Should set bidders", function () {
+    return auctionInstance.register({ from: accounts[1] }).then(function (result) {
+      return auctionInstance.getPersonDetails(0);
+    }).then(function (result) {
       assert.equal(result[2], accounts[1], 'bidder address set');
     })
   });
 
   //Test Case for checking if the bid is more than the token amount
-  it("Should NOT allow to bid more than remaining tokens", function() {
+  it("Should NOT allow to bid more than remaining tokens", () => {
     /**********
     TASK 1:   Call bid method from accounts[1] of Auction.sol using auctionInstance and
     pass itemId=0, count=6 as arguments
     HINT:     To make a function call from account 1 use {from: accounts[1]} as an extra argument
     ***********/
-    return /*<CODE HERE>*/
-    .then(function (result) {
-      /*
-      We are testing for a negative condition and hence this particular block will not have executed if our test case was correct. If this part is executed then we throw an error and catch the error to assert false
-      */
-      throw("Failed to check remaining tokens less than count");
-    }).catch(function (e) {
-      var a = e.toString();
-      if(e === "Failed to check remaining tokens less than count") {
-        /**********
-        TASK 2: This is the error which we had thrown. Should you assert true or false?
-        HINT:   Use assert(false) to assert false
-                Use assert(true) to assert true
-        ***********/
-        /*<CODE HERE>*/
-      } else {
-        /**********
-        TASK 3: assert the opposite here
-        ***********/
-        /*<CODE HERE>*/
-      }
-    })
+    return auctionInstance.bid({ from: accounts[1], itemId: 0, count: 6 })
+      .then((result) => {
+        /*
+        We are testing for a negative condition and hence this particular block will not have executed if our test case was correct. If this part is executed then we throw an error and catch the error to assert false
+        */
+        throw ("Failed to check remaining tokens less than count");
+      }).catch(function (e) {
+        var a = e.toString();
+        if (e === "Failed to check remaining tokens less than count") {
+          /**********
+          TASK 2: This is the error which we had thrown. Should you assert true or false?
+          HINT:   Use assert(false) to assert false
+                  Use assert(true) to assert true
+          ***********/
+          /*<CODE HERE>*/
+          assert(false) = false;
+          assert(true) = true;
+        } else {
+          /**********
+          TASK 3: assert the opposite here
+          ***********/
+          /*<CODE HERE>*/
+          assert(false) = true;
+          assert(true) = false;
+        }
+      })
   });
 
   //Modifier Checking
-  it("Should NOT allow non owner to reveal winners", function() {
+  it("Should NOT allow non owner to reveal winners", function () {
     /**********
     TASK 4: Call revealWinners from account 1
     ***********/
-     return /*<CODE HERE>*/
-     .then(function (instance) {
-       /*
-       We are testing for a negative condition and hence this particular block will not have executed if our test case was correct. If this part is executed then we throw an error and catch the error to assert false
-       */
-       throw("Failed to check owner in reveal winners");
-     }).catch(function (e) {
-       if(e === "Failed to check owner in reveal winners") {
-         /**********
-         TASK 5: This is the error which we had thrown. Should you assert true or false?
-         HINT:   Use assert(false) to assert false
-                 Use assert(true) to assert true
-         ***********/
-         /*<CODE HERE>*/
-       } else {
-         /**********
-         TASK 6: assert the opposite here
-         ***********/
-         /*<CODE HERE>*/
-       }
-     })
-   })
+    return auctionInstance.revealOwner({ from: accounts[0] })
+      .then(function (instance) {
+        /*
+        We are testing for a negative condition and hence this particular block will not have executed if our test case was correct. If this part is executed then we throw an error and catch the error to assert false
+        */
+        throw ("Failed to check owner in reveal winners");
+      }).catch(function (e) {
+        if (e === "Failed to check owner in reveal winners") {
+          /**********
+          TASK 5: This is the error which we had thrown. Should you assert true or false?
+          HINT:   Use assert(false) to assert false
+                  Use assert(true) to assert true
+          ***********/
+          /*<CODE HERE>*/
+          assert(false) = false;
+          assert(true) = true;
+        } else {
+          /**********
+          TASK 6: assert the opposite here
+          ***********/
+          /*<CODE HERE>*/
+          assert(false) = true;
+          assert(true) = false;
+        }
+      })
+  })
 
 
-  it("Should set winners", function() {
+  it("Should set winners", function () {
     /**********
     TASK 7: Call register function from account 2
     ***********/
     return /*<CODE HERE>*/
-    .then(function(result) {
+    .then(function (result) {
       /**********
       TASK 8: Call register function from account 3
       ***********/
-        return /*<CODE HERE>*/
-    }).then(function() {
+      return /*<CODE HERE>*/
+    }).then(function () {
       /**********
       TASK 9: Call register function from account 4
       ***********/
-        return /*<CODE HERE>*/
-    }).then(function() {
+      return /*<CODE HERE>*/
+    }).then(function () {
       /**********
       TASK 10: Call bid method from accounts[2] of Auction.sol using auctionInstance and
       pass itemId=0, count=5 as arguments
       ***********/
-        return /*<CODE HERE>*/
-    }).then(function() {
+      return /*<CODE HERE>*/
+    }).then(function () {
       /**********
       TASK 11: Call bid method from accounts[3] of Auction.sol using auctionInstance and
       pass itemId=1, count=5 as arguments
       ***********/
-        return /*<CODE HERE>*/
-    }).then(function() {
+      return /*<CODE HERE>*/
+    }).then(function () {
       /**********
       TASK 12: Call bid method from accounts[4] of Auction.sol using auctionInstance and
       pass itemId=2, count=5 as arguments
       ***********/
-        return /*<CODE HERE>*/
-    }).then(function() {
+      return /*<CODE HERE>*/
+    }).then(function () {
       /**********
       TASK 13: Call revealWinners function from accounts[0]
       ***********/
-        return /*<CODE HERE>*/
-    }).then(function() {
+      return /*<CODE HERE>*/
+    }).then(function () {
       /**********
       TASK 14: call winners function from accounts[0] to get the winner of item id 0
       ***********/
-        return /*<CODE HERE>*/
-    }).then(function(result) {
+      return /*<CODE HERE>*/
+    }).then(function (result) {
       /**********
       TASK 15:  assert to see if the winner address is not the default address
       HINT:     Default address is '0x0000000000000000000000000000000000000000'
@@ -138,7 +146,7 @@ contract('AuctionContract', function (accounts) {
       TASK 16: call winners function from accounts[0] to get the winner of item id 1
       ***********/
       return /*<CODE HERE>*/
-    }).then(function(result) {
+    }).then(function (result) {
       /**********
       TASK 17:  assert to see if the winner address is not the default address
       HINT:     Default address is '0x0000000000000000000000000000000000000000'
@@ -150,7 +158,7 @@ contract('AuctionContract', function (accounts) {
       TASK 18: Call winners function from account 3 to get the winner of item id 2
       ***********/
       return /*<CODE HERE>*/
-    }).then(function(result) {
+    }).then(function (result) {
       /**********
       TASK 19:  assert to see if the winner address is not the default address
       HINT:     Default address is '0x0000000000000000000000000000000000000000'
